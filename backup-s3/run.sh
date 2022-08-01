263f210272e3f381ec02371e5b0e17953a12ae9c
Lior Perez <pertoul@gmail.com>
2022-08-01 15:12:02 +0200

    Remove --delete

--- backup-s3/run.sh
+++ backup-s3/run.sh
@@ -13,6 +13,6 @@ aws configure set aws_secret_access_key $SECRET
 
 aws configure set s3.max_concurrent_requests 1
 
-aws s3 sync /backup/ s3://$BUCKET/  --delete
+aws s3 sync /backup/ s3://$BUCKET/
 
 echo "Done"
